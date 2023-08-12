15d338bf54095193c0bf765f0d696dbfc9cc610a
Aishwarya <aishwarya.b.1210@gmail.com>
2023-08-12 13:41:22 +0530

    adding files

---
+++ src/FirstNonRepCharacter.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class FirstNonRepCharacter {
+    public static void main(String[] args) {
+        String s = "Java documents are best";
+        Character firstNonRepeating =
+                s.chars()
+                .mapToObj(e->Character.toLowerCase((char)e))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                        .entrySet()
+                        .stream()
+                        .filter(x->x.getValue()==1L)
+                        .map(x->x.getKey())
+                        .findFirst().get();
+        System.out.println("firstNonRepeating Chanracter is = "+firstNonRepeating);
+    }
+
+}
